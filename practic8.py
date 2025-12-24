903165dbfcff72502e91f5e9ca1bfd89ddea9106
Marri-web <mypresentations29@gmail.com>
2025-12-24 02:30:40 +0200

    Update practic8.py
    Відповідь на запитання, а також створення нового запитання наступному студенту

--- practic8.py
+++ practic8.py
@@ -9,3 +9,19 @@ def student1_create_file(filename):
     except IOError as e:
         print("Помилка створення файлу:", e)
 
+
+# ---------- Студент 2: Грибоєдова Марина ----------
+# Додавання відповіді та нового питання
+
+def student2_add_answer(filename):
+    try:
+        with open(filename, "a", encoding="utf-8") as file:
+            file.write("Студент 2: Грибоєдова М\n")
+            file.write("Відповідь:\n")
+            file.write("Словники (dict) в Python — це структура даних,\n")
+            file.write("яка зберігає пари ключ–значення.\n")
+            file.write("Вони є змінюваними та зручними для роботи з даними.\n")
+            file.write("Питання: Чим словники відрізняються від списків?\n\n")
+        print("Студент 2: відповідь додано")
+    except IOError as e:
+        print("Помилка запису у файл:", e)
